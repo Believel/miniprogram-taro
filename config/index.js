0de82f5c7410defa928383f5d376902b6ea92c49
panp.zhang001 <panp.zhang001@bkjk.com>
2020-09-16 18:44:00 +0800

    config

--- config/index.js
+++ config/index.js
@@ -1,24 +1,38 @@
+const path = require('path');
 const config = {
   projectName: 'myTaro',
   date: '2020-9-10',
   designWidth: 750,
+  // 设计稿尺寸换算规则
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2
   },
+  // 项目源码目录
   sourceRoot: 'src',
+  // 项目产出目录
   outputRoot: 'dist',
+  // 编译插件配置
   plugins: [],
+  // 全局变量设置
   defineConstants: {
+    A: '"a"'
   },
+  // 别名
+  alias: {
+    '@/pages': path.resolve(__dirname, '..', 'src/pages')
+  },
+  // 文件 copy 配置
   copy: {
     patterns: [
     ],
     options: {
     }
   },
+  // 框架：react、nerv、vue、vue3等
   framework: 'react',
+  // 小程序端专用设置
   mini: {
     postcss: {
       pxtransform: {
@@ -27,6 +41,7 @@ const config = {
 
         }
       },
+      // 小程序端样式引用本地资源内联配置
       url: {
         enable: true,
         config: {
@@ -40,8 +55,11 @@ const config = {
           generateScopedName: '[name]__[local]___[hash:base64:5]'
         }
       }
-    }
+    },
+    // 自定义 webpack 配置
+    webpackChain: {}
   },
+  // h5 端专用配置
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
--- src/pages/demo/demo.tsx
+++ src/pages/demo/demo.tsx
@@ -4,15 +4,27 @@ import { getCurrentInstance } from "@tarojs/taro";
 
 import "./demo.less";
 
+type PageState = {
+    data: Data
+}
+interface Data {
+    type?: string;
+    id?: string;
+}
 class Demo extends Component {
+    state: PageState = {
+        data: {}
+    }
     componentDidMount() {
         // 获取路由上传入的参数
-        console.log(getCurrentInstance().router?.params);
+        this.setState({
+            data: getCurrentInstance().router?.params
+        })
     }
     render() {
         return (
             <View className="demo">
-                <Text>demo</Text>
+                <Text>读取路由地址中传的参数：type = {this.state.data?.type} ,id = {this.state.data?.id}</Text>
             </View>
         )
     }
